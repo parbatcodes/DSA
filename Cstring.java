a2312ad00e535c9433f9a8cd59657d2756a6b684
Parbat Singh <99901618+parbatcodes@users.noreply.github.com>
2022-12-27 10:37:27 +0530

    Add files via upload
    Here I created a program to get individual characters from a string

---
+++ Cstring.java
@@ -0,0 +1,9 @@
+public class Cstring {
+    public static void main(String[] args) {
+        String s = "Parbat";
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i) + " ");
+        }
+    }
+
+}
